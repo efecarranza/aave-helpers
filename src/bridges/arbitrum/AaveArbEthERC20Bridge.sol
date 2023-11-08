61291a8ae6e642308cbb9e2e979e0fcb31b3ade4
efecarranza <fcarr004@fiu.edu>
2023-11-08 10:36:03 -0500

    feat: scaffold arb bridge

---
+++ src/bridges/arbitrum/AaveArbEthERC20Bridge.sol
@@ -0,0 +1,10 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {IAaveArbEthERC20Bridge} from './IAaveArbEthERC20Bridge.sol';
+
+contract AaveArbEthERC20Bridge is IAaveArbEthERC20Bridge {
+
+}
