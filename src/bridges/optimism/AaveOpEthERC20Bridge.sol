34ec0b2307d24475f5f00ce16a721345c112a0b0
efecarranza <fcarr004@fiu.edu>
2023-11-13 09:54:56 -0500

    chore: scaffold opt

---
+++ src/bridges/optimism/AaveOpEthERC20Bridge.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
+import {SafeERC20} from 'solidity-utils/contracts/oz-common/SafeERC20.sol';
+import {Ownable} from 'solidity-utils/contracts/oz-common/Ownable.sol';
+import {Rescuable} from 'solidity-utils/contracts/utils/Rescuable.sol';
+import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
+
+import {ChainIds} from '../../ChainIds.sol';
+import {IAaveOpEthERC20Bridge} from './IAaveOpEthERC20Bridge.sol';
+
+contract AaveOpEthERC20Bridge is Ownable, Rescuable, IAaveOpEthERC20Bridge {
+
+}
