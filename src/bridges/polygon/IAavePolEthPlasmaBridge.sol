4ef9823f42f5c283bc9811b6fc446d43187fb5f8
efecarranza <fcarr004@fiu.edu>
2024-02-26 20:59:49 -0500

    chore: lint

--- src/bridges/polygon/IAavePolEthPlasmaBridge.sol
+++ src/bridges/polygon/IAavePolEthPlasmaBridge.sol
@@ -3,10 +3,10 @@
 pragma solidity ^0.8.0;
 
 interface IAavePolEthPlasmaBridge {
-   /// This function withdraws an ERC20 token from Polygon to Mainnet. exit() needs
-   /// to be called on mainnet with the corresponding burnProof in order to complete.
-   /// @notice Polygon only. Function will revert if called from other network.
-   /// @param token Polygon address of ERC20 token to withdraw
-   /// @param amount Amount of tokens to withdraw
-    function bridge(address token, uint256 amount) external;
+  /// This function withdraws an ERC20 token from Polygon to Mainnet. exit() needs
+  /// to be called on mainnet with the corresponding burnProof in order to complete.
+  /// @notice Polygon only. Function will revert if called from other network.
+  /// @param token Polygon address of ERC20 token to withdraw
+  /// @param amount Amount of tokens to withdraw
+  function bridge(address token, uint256 amount) external;
 }
